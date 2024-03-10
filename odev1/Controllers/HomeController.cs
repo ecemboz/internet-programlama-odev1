264b673f254793f3ae1e53508b586fafbcd38096
user <user@DESKTOP-3OCN56F>
2024-03-10 14:03:11 +0300

    Proje dosyası ekle.

---
+++ odev1/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using odev1.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace odev1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "iris.json");
+            string json = System.IO.File.ReadAllText(jsonFilePath);
+
+            JArray jsonArray = JArray.Parse(json);
+            List<object> irisDataList = new List<object>();
+            foreach (var item in jsonArray)
+            {
+                irisDataList.Add(item);
+            }
+
+            return View(irisDataList);
+        }
+    }
+}
---
+++ odev1/Models/IrisData.cs
@@ -0,0 +1,11 @@
+﻿namespace odev1.Models
+{
+    internal class IrisData
+    {
+        public float SepalLength { get; set; }
+        public float SepalWidth { get; set; }
+        public float PetalLength { get; set; }
+        public float PetalWidth { get; set; }
+        public string Species { get; set; }
+    }
+}
